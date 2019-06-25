23b345cfdcf27019875bfc25480545c530edc1a9
ejemplolaravel <jomabv79@gmail.com>
2019-06-25 08:26:38 -0500

    tabla usuario creada

---
+++ database/migrations/2019_06_25_124445_crear_tabla_usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tipo_documento',20);
+            $table->string('documento',20);
+            $table->string('nombre',50);
+            $table->string('apellido',50);
+            $table->string('telefono',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
